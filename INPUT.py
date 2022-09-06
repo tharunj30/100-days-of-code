4f958ab1aa3d687b88d82575970a305e785cf30c
Tharun S J <tharunkumar200230@gmail.com>
2022-09-06 16:20:46 +0530

    Add files via upload

---
+++ INPUT.py
@@ -0,0 +1,16 @@
+#null is aka none in python
+
+#numeric types->int float boolean complex
+#logical operaors and or not
+# x = int(input("enter a number"))
+# y=int(input("enter a number"))
+# print(x+y)
+# ch=input("enter a character ")
+# print(ch[0])
+# result=eval(input("enter an exprs"))
+# print(result)
+import sys 
+x=int(sys.argv[1])
+y=int(sys.argv[2])
+z=x+y
+print(z)
---
+++ ifelse.py
@@ -0,0 +1,11 @@
+x=3
+r=x%2
+if r==0:
+    print("its even")
+    if x>8:
+        print("wow")
+    else:
+        print("no wow")
+else:
+    print("odd hei re")
+    
